cb151bbabf0f3513e4c16f5c408eed0a6e76bcc7
spences10 <spences10apps@gmail.com>
2020-07-31 19:02:23 +0100

    reactive declarations and statements

--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { fade } from "svelte/transition";
   import Qusetion from "./Question.svelte";
 
   let quiz = getQuiz();
@@ -26,20 +27,38 @@
   function addToScore() {
     score = score + 1;
   }
+
+  // Reactive statement
+  $: if (score > 7) {
+    alert(`You won! yay 🎉`);
+    resetQuiz();
+  }
+
+  // Reactive declaration
+  $: questionNumber = activeQuestion + 1;
 </script>
 
-<button on:click={resetQuiz}>Get new Questions</button>
-<h3>Score: {score}</h3>
-<h4>Question #{activeQuestion + 1}</h4>
+<style>
+  /* text-align: center; */
+  .fade-wrapper {
+    position: relative;
+    transition: all 300ms;
+  }
+</style>
 
 <div>
+  <button on:click={resetQuiz}>Get new Questions</button>
+  <h3>Score: {score}</h3>
+  <h4>Question #{questionNumber}</h4>
 
   {#await quiz}
     Loading...
   {:then data}
     {#each data.results as question, index}
       {#if index === activeQuestion}
-        <Qusetion {question} {nextQuestion} {addToScore} />
+        <div transition:fade class="fade-wrapper">
+          <Qusetion {question} {nextQuestion} {addToScore} />
+        </div>
       {/if}
     {/each}
   {/await}
